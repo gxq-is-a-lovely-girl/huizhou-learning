26cd93adc163d6b8040d8c61962c3d0100a3ed1f
cmathFPV <84088992+gxq-is-a-lovely-girl@users.noreply.github.com>
2021-08-14 09:46:42 +0800

    Update 迷宫.cpp

--- 迷宫.cpp
+++ 迷宫.cpp
@@ -7,16 +7,16 @@
 using namespace std;
 int n,m;
 int  map[10][10]={0};
-bool visit[10][10]={0};
+bool visit[10][10];
 int  sx,sy,ex,ey;
 int dx[5]={0,1,0,-1,0};
 int dy[5]={0,0,1,0,-1};
 int ans=0;
 bool check(int x,int y){
-	if(visit[x][y]==false&&map[x][y]==0&&x<n&&y<m&&x>=0&&y>=0){
-		return true;
-	}else{
+	if(visit[x][y]==true||map[x][y]==0||x>n||y>m||x<1||y<1){
 		return false;
+	}else{
+		return true;
 	}
 }
 void tmp(int x,int y){
@@ -34,20 +34,24 @@ void tmp(int x,int y){
 	}
 }
 int main(){
-	int k,l;
-	cin>>n>>m;
+	int k,l;	
 	int t;
+	cin>>n>>m>>t;
 	cin>>sx>>sy>>ex>>ey;
 	for(int i=0;i<=9;i++){
 		for(int j=0;j<=9;j++){
 			visit[i][j]=false;
 		}
 	}
+	for(int i=0;i<10;i++){
+		for(int j=0;j<10;j++){
+			map[i][j]=1;
+		}
+	}
 	for(int i=1;i<=t;i++){
 		cin>>k>>l;
-		map[k][l]=1;
+		map[k][l]=0;
 }
 	tmp(sx,sy); 
 		cout<<ans;
 }
-
